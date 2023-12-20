56a2c58108ffcf39b9f10846c7839a6a5f86a058
박수정 <zxzx4342@naver.com>
2023-12-20 18:46:26 +0900

    BaseActivity에 테스트용 사이드바 추가

--- app/src/main/java/com/crystal/todayprice/component/BaseActivity.kt
+++ app/src/main/java/com/crystal/todayprice/component/BaseActivity.kt
@@ -3,22 +3,27 @@ package com.crystal.todayprice.component
 import android.content.Intent
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.view.GravityCompat
 import androidx.core.view.isVisible
 import com.bumptech.glide.Glide
 import com.crystal.todayprice.MainActivity
 import com.crystal.todayprice.R
 import com.crystal.todayprice.databinding.ActivityBaseBinding
+import com.crystal.todayprice.repository.TAG
+import com.google.android.material.navigation.NavigationView
 
 
 open class BaseActivity(
     private val toolbarType: ToolbarType,
     private val transitionMode: TransitionMode = TransitionMode.NONE
-) : AppCompatActivity() {
+) : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     protected lateinit var baseBinding: ActivityBaseBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,13 +32,16 @@ open class BaseActivity(
         setContentView(baseBinding.root)
         setSupportActionBar(baseBinding.toolbar)
         setToolbar()
+        baseBinding.navigationView.setNavigationItemSelectedListener(this)
 
         when (transitionMode) {
             TransitionMode.HORIZON -> overridePendingTransition(R.anim.horizon_enter, R.anim.none)
             TransitionMode.VERTICAL -> overridePendingTransition(R.anim.vertical_enter, R.anim.none)
             else -> {}
         }
+
     }
+
 
     override fun finish() {
         super.finish()
@@ -46,63 +54,52 @@ open class BaseActivity(
     }
 
     override fun onBackPressed() {
-        super.onBackPressed()
+        if (baseBinding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            baseBinding.drawerLayout.closeDrawers()
+        } else {
+            super.onBackPressed()
 
-        if (isFinishing) {
-            when (transitionMode) {
-                TransitionMode.HORIZON -> overridePendingTransition(
-                    R.anim.none,
-                    R.anim.horizon_exit
-                )
-
-                TransitionMode.VERTICAL -> overridePendingTransition(
-                    R.anim.none,
-                    R.anim.vertical_exit
-                )
-
-                else -> Unit
+            if (isFinishing) {
+                when (transitionMode) {
+                    TransitionMode.HORIZON -> overridePendingTransition(
+                        R.anim.none,
+                        R.anim.horizon_exit
+                    )
+                    TransitionMode.VERTICAL -> overridePendingTransition(
+                        R.anim.none,
+                        R.anim.vertical_exit
+                    )
+                    else -> Unit
+                }
             }
         }
     }
 
     override fun onDestroy() {
+        baseBinding.navigationView.setNavigationItemSelectedListener(null)
         super.onDestroy()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.action_search -> {
-                Toast.makeText(this, "검색하기", Toast.LENGTH_SHORT).show()
+                actionMenuSearch()
             }
 
             R.id.action_favorite -> {
-                Toast.makeText(this, "즐겨찾기", Toast.LENGTH_SHORT).show()
+                actionMenuFavorite()
             }
 
             R.id.action_home -> {
-                val intent = Intent(this, MainActivity::class.java)
-                val stackIntent = packageManager.getLaunchIntentForPackage(packageName)
-                if (stackIntent != null) {
-                    startActivity(stackIntent)
-                } else {
-                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                    startActivity(intent)
-                }
-                finish()
+                actionMenuHome()
             }
 
             android.R.id.home -> {
-                if (toolbarType == ToolbarType.MENU) {
-                    Toast.makeText(this, "메뉴", Toast.LENGTH_SHORT).show()
-                }
-                if (toolbarType == ToolbarType.BACK) {
-                    finish()
-                }
+                actionHome()
             }
         }
         return super.onOptionsItemSelected(item)
     }
-
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         when (toolbarType) {
             ToolbarType.MENU -> menuInflater.inflate(R.menu.menu_with_search, menu)
@@ -110,7 +107,6 @@ open class BaseActivity(
         }
         return true
     }
-
     private fun setToolbar() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setBackgroundDrawable(
@@ -151,6 +147,43 @@ open class BaseActivity(
         )
     }
 
+    private fun actionMenuSearch() {
+        Toast.makeText(this, "검색하기", Toast.LENGTH_SHORT).show()
+    }
+    private fun actionMenuFavorite() {
+        Toast.makeText(this, "즐겨찾기", Toast.LENGTH_SHORT).show()
+    }
+    private fun actionMenuHome() {
+        val intent = Intent(this, MainActivity::class.java)
+        val stackIntent = packageManager.getLaunchIntentForPackage(packageName)
+        if (stackIntent != null) {
+            startActivity(stackIntent)
+        } else {
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+        finish()
+    }
+    private fun actionHome() {
+        when (toolbarType) {
+            ToolbarType.MENU -> {
+                baseBinding.drawerLayout.openDrawer(GravityCompat.START)
+
+            }
+            ToolbarType.BACK -> {
+                finish()
+            }
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menuItem1 -> Toast.makeText(this, "item1 clicked", Toast.LENGTH_SHORT).show()
+            R.id.menuItem2 -> Toast.makeText(this, "item2 clicked", Toast.LENGTH_SHORT).show()
+            R.id.menuItem3 -> Toast.makeText(this, "item3 clicked", Toast.LENGTH_SHORT).show()
+        }
+        return false
+    }
 }
 
 enum class ToolbarType {
